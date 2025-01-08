2234be03f58c5bbbfb9bba3bc9346a9e73a2cf36
peardox <simon@peardox.com>
2025-01-08 10:06:01 +0000

    Add Windefs

--- src/unit1.pas
+++ src/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
   { ~/Library/Application Support/Steam/libraryfolders.vdf }
   { C:\Program Files (x86)\Steam\config\libraryfolders.vdf }
 type
@@ -13,7 +13,9 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    Memo1: TMemo;
     Panel1: TPanel;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -26,6 +28,32 @@ var
 implementation
 
 {$R *.lfm}
+
+function ReadTextFile(const AFilename: String): String;
+var
+  F: TFileStream;
+begin
+  Result := String.Empty;
+  if(FileExists(AFilename)) then
+    begin
+      F := TFileStream.Create(AFilename, fmOpenRead, fmShareDenyNone);
+      F.Seek(0, soFromBeginning);
+      SetLength(Result, F.Size);
+      if F.Size <> 0 then
+        F.ReadBuffer(Result[1], Length(Result));
+    end;
+end;
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  {$if defined(WINDOWS)}
+  Memo1.Text := ReadTextFile('C:\\Program Files (x86)\\Steam\\config\\libraryfolders.vdf');
+  {$elseif defined(MACOSX)}
+  Memo1.Text := ReadTextFile('~/Library/Application Support/Steam/libraryfolders.vdf');
+  {$endif}
+end;
 
 end.
 
